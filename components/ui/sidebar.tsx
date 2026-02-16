2239c95011e097f333f01e1b402d3d2c9e70f75f
Hau Pham <phaucau2311@gmail.com>
2026-02-16 20:10:50 +0700

    comiit fix rebuild

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -24,7 +24,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip"
-
+//aloo
 const SIDEBAR_COOKIE_NAME = "sidebar_state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
 const SIDEBAR_WIDTH = "16rem"
